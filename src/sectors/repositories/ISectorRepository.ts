a9466fcf8473ecbbb13f49f51328f1f765341e49
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-11-07 15:45:11 -0300

    feat: create repository gateway

---
+++ src/sectors/repositories/ISectorRepository.ts
@@ -0,0 +1,11 @@
+import { CreateSectorDto } from "../dto/create-sector.dto";
+import { UpdateSectorDto } from "../dto/update-sector.dto";
+import { Sector } from "../entities/sector.entity";
+
+export interface ISectorRepository {
+	create(sector: CreateSectorDto): Promise<Sector>;
+	update(sector: UpdateSectorDto): Promise<Sector>;
+	findAll(): Promise<Sector[]>;
+	findOne(id: string): Promise<Sector>;
+	delete(id: string): Promise<Sector>;
+}
