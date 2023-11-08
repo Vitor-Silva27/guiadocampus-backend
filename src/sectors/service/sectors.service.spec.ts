e97f0fade10a59ca9a7651c9ec23afdf7daf5f0a
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-11-08 10:47:47 -0300

    test: add test to find sector by id

--- src/sectors/service/sectors.service.spec.ts
+++ src/sectors/service/sectors.service.spec.ts
@@ -13,28 +13,43 @@ describe("SectorsService", () => {
 		service = new SectorsService(repository);
 	});
 
-	it("should create a sector", async () => {
-		const sector = await service.create(validSector);
-		expect(sector.name).toBe(validSector.name);
+	describe("Sectors creation suite", () => {
+		it("should create a sector", async () => {
+			const sector = await service.create(validSector);
+			expect(sector.name).toBe(validSector.name);
+		});
+
+		it("should not create a sector without name", async () => {
+			expect(async () => {
+				await service.create(sectorWithoutName);
+			}).rejects.toThrow("Could not create a sector without a name!");
+		});
+
+		it("should not create a sector that already exists", async () => {
+			expect(async () => {
+				await service.create(validSector);
+				await service.create(validSector);
+			}).rejects.toThrow("Could not create a sector that already exists!");
+		});
 	});
 
-	it("should not create a sector without name", async () => {
-		expect(async () => {
-			await service.create(sectorWithoutName);
-		}).rejects.toThrow("Could not create a sector without a name!");
-	});
-
-	it("should not create a sector that already exists", async () => {
-		expect(async () => {
+	describe("Sectors find all suite", () => {
+		it("should find all sectors", async () => {
 			await service.create(validSector);
-			await service.create(validSector);
-		}).rejects.toThrow("Could not create a sector that already exists!");
+			const sectors = await service.findAll();
+			expect(sectors.length).toBeGreaterThan(0);
+			expect(sectors[0].name).toBe(validSector.name);
+		});
 	});
 
-	it("should find all sectors", async () => {
-		await service.create(validSector);
-		const sectors = await service.findAll();
-		expect(sectors.length).toBeGreaterThan(0);
-		expect(sectors[0].name).toBe(validSector.name);
+	describe("Sectors find one suite", () => {
+		it("should find a sector by id", async () => {
+			const newSector = await service.create(validSector);
+			const foundSector = await service.findOne(newSector.id);
+			console.log("newSector", newSector);
+			console.log("foundSector", foundSector);
+
+			expect(foundSector.name).toBe(newSector.name);
+		});
 	});
 });
