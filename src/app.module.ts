04c54bc2ecdcd871b1617abcf6c696950366522d
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-11-08 10:48:16 -0300

    style: format spaces

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import { SectorsModule } from './sectors/sectors.module';
+import { SectorsModule } from "./sectors/sectors.module";
 @Module({
 	imports: [ConfigModule.forRoot(), SectorsModule],
 	controllers: [AppController],
