609a6afad20b67a41b42f763cea11c5589c8cfeb
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2023-11-22 07:24:41 -0300

    feat: generate admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { SectorsModule } from "./sectors/sectors.module";
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 @Module({
-	imports: [ConfigModule.forRoot(), SectorsModule],
+	imports: [ConfigModule.forRoot(), SectorsModule, AuthModule, AdminModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ async function bootstrap() {
 			"Bem-vindo à documentação da API do 'Guia do Campus'! Nossa API proporciona acesso fácil a informações essenciais sobre departamentos, salas de aula, serviços, horários e eventos na universidade, simplificando a experiência dos alunos, professores e funcionários. Explore a documentação para obter detalhes sobre como usar a API.",
 		)
 		.setVersion("0.0.1")
+		.addTag("sectors")
+		.addTag("authentication")
+		.addTag("admin")
 		.build();
 
 	const document = SwaggerModule.createDocument(app, config);
